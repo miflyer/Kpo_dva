47ec046cb9dbb52736b6587843bc66cab80e4427
MiFlyer <miflyer98@gmail.com>
2026-09-04 15:14:42 +0300

    added cache decorator

--- main.py
+++ main.py
@@ -1,4 +1,62 @@
 from serialization import Serialization
+import hashlib
+import os
+import pickle
+from functools import wraps
+import time
+
+def timer(func):
+    @wraps(func)  # Сохраняет имя и документацию исходной функции
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Функция {func.__name__} выполнилась за {end_time - start_time:.4f} сек.")
+        return result
+    return wrapper
+
+def get_cache_key(func_name, args, kwargs):
+    # Создаем строку из аргументов и хешируем её
+    key_str = f"{func_name}{args}{tuple(sorted(kwargs.items()))}"
+    return hashlib.md5(key_str.encode()).hexdigest()
+
+# Простой декоратор для кэширования
+def cache_func(cache_dir=".cache"):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # Создаем директорию для кэша
+            os.makedirs(cache_dir, exist_ok=True)
+
+            # Генерируем имя файла кэша
+            key = get_cache_key(func.__name__, args, kwargs)
+            cache_file = os.path.join(cache_dir, f"{key}.pkl")
+
+            # Пытаемся прочитать из кэша
+            if os.path.exists(cache_file):
+                with open(cache_file, 'rb') as f:
+                    return pickle.load(f)
+
+            # Если кэша нет - выполняем функцию
+            result = func(*args, **kwargs)
+
+            # Сохраняем результат
+            with open(cache_file, 'wb') as f:
+                pickle.dump(result, f)
+
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+@cache_func()
+@timer
+def slow_func(x):
+    time.sleep(5)
+    ans = x ** x * x ** x
+    return ans
 
 class Combined_data():
     def __init__(self, list_of, dict_of):
@@ -37,12 +95,15 @@ def main():
     print("\n\nЗагруженное дерево:")
     Serialization.print_tree(restored_tree)
 
-
     current_win = Window_size(300, 400)
     Serialization.serialize_to_file(current_win, "current_win.pkl")
     loaded_win = Serialization.deserialize_from_file("current_win.pkl")
-    print(f"\n\nLoaded window size:{current_win.height}x{current_win.length}")
+    print(f"\n\nLoaded window size:{loaded_win.height}x{loaded_win.length}")
 
+
+    print(slow_func(15))
+    print(slow_func(15))
+    print(slow_func(15))
 
 if __name__ == '__main__':
     main()
--- serialization.py
+++ serialization.py
@@ -1,5 +1,6 @@
 import pickle
 
+
 class TreeNode:
     def __init__(self, value):
         self.value = value  # Значение узла
